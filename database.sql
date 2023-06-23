65c47964b48bbb18caa5c8230c865509b0b2a1a1
Emil Murzin <emil.morua@gmail.com>
2023-06-23 15:54:30 +0300

    update database.sql

--- database.sql
+++ database.sql
@@ -3,3 +3,13 @@ CREATE TABLE urls (
   name VARCHAR(255),
   created_at TIMESTAMP
 );
+
+CREATE TABLE url_checks (
+  id SERIAL PRIMARY KEY,
+  url_id INT REFERENCES urls(id),
+  status_code INT,
+  h1 VARCHAR(255),
+  title VARCHAR(255),
+  description VARCHAR(255),
+  created_at TIMESTAMP
+);
