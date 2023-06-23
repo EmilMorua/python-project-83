4972996e901f37b99719ae9717ef3ac5ffcb9e47
Emil Murzin <emil.morua@gmail.com>
2023-06-23 11:34:00 +0300

    update app.py

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,8 +1,11 @@
 from flask import Flask
+from flask import render_template
 
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Welcome to Flask!'
+def index():
+    return render_template(
+        'index.html',
+    )
