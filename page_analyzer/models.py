f75c4412761e1c8594cac1e8da5d664d0bd6b528
Emil Murzin <emil.morua@gmail.com>
2023-06-23 15:54:18 +0300

    update models.py

--- page_analyzer/models.py
+++ page_analyzer/models.py
@@ -10,3 +10,19 @@ class Url(db.Model):
 
     def __init__(self, name):
         self.name = name
+
+
+class UrlCheck(db.Model):
+    __tablename__ = 'url_checks'
+
+    id = db.Column(db.Integer, primary_key=True)
+    url_id = db.Column(db.Integer, db.ForeignKey('urls.id'))
+    status_code = db.Column(db.Integer)
+    h1 = db.Column(db.String(255))
+    title = db.Column(db.String(255))
+    description = db.Column(db.String(255))
+    created_at = db.Column(db.DateTime, default=db.func.current_timestamp())
+
+    def __init__(self, url_id, created_at):
+        self.url_id = url_id
+        self.created_at = created_at
