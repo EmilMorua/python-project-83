1e61023c4d92e60f3c0c9767f093763f3d5216e8
Emil Murzin <emil.morua@gmail.com>
2023-06-28 13:12:31 +0300

    update forms.py

--- page_analyzer/forms.py
+++ page_analyzer/forms.py
@@ -2,6 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import URL
 
+
 class URLForm(FlaskForm):
     url = StringField('URL', validators=[URL()])
     submit = SubmitField('Submit')
