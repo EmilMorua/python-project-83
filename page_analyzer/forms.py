ade4b8cf0047b254b15afe711b791d6dec6a5da3
Emil Murzin <emil.morua@gmail.com>
2023-07-13 12:52:01 +0500

    update forms.py

--- page_analyzer/forms.py
+++ page_analyzer/forms.py
@@ -1,8 +1,9 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import URL
-
+from flask_wtf.csrf import CSRFTokenField
 
 class URLForm(FlaskForm):
     url = StringField('URL', validators=[URL()])
     submit = SubmitField('Submit')
+    csrf_token = CSRFTokenField()
