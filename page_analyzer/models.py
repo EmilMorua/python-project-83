a36b5c39092ecc4c8731e3f6e0651110f8a04e43
Emil Murzin <emil.morua@gmail.com>
2023-07-13 11:29:03 +0500

    update models.py

--- page_analyzer/models.py
+++ page_analyzer/models.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from page_analyzer.extensions import db
 
 
@@ -6,23 +7,27 @@ class Url(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255))
-    created_at = db.Column(db.DateTime, default=db.func.current_timestamp())
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
 
-    def __init__(self, name):
-        self.name = name
+    def __repr__(self) -> str:
+        return f'<Url {self.id}>'
 
 
 class UrlCheck(db.Model):
     __tablename__ = 'url_checks'
 
     id = db.Column(db.Integer, primary_key=True)
-    url_id = db.Column(db.Integer, db.ForeignKey('urls.id'))
+    url_id = db.Column(db.Integer, db.ForeignKey('urls.id'), nullable=False)
+    created_at = db.Column(
+        db.DateTime, nullable=False,
+        default=datetime.utcnow
+    )
+    h1_content = db.Column(db.String(255))
+    title_content = db.Column(db.String(255))
+    description_content = db.Column(db.String(255))
     status_code = db.Column(db.Integer)
-    h1 = db.Column(db.String(255))
-    title = db.Column(db.String(255))
-    description = db.Column(db.String(255))
-    created_at = db.Column(db.DateTime, default=db.func.current_timestamp())
 
-    def __init__(self, url_id, created_at):
-        self.url_id = url_id
-        self.created_at = created_at
+    url = db.relationship('Url', backref=db.backref('checks', lazy=True))
+
+    def __repr__(self) -> str:
+        return f'<UrlCheck {self.id}>'
