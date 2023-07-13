c0002819270e49de96ffbdf33b6e752d6b05e298
Emil Murzin <emil.morua@gmail.com>
2023-07-13 11:28:37 +0500

    update database.sql

--- database.sql
+++ database.sql
@@ -8,8 +8,8 @@ CREATE TABLE url_checks (
   id SERIAL PRIMARY KEY,
   url_id INT REFERENCES urls(id),
   status_code INT,
-  h1 VARCHAR(255),
-  title VARCHAR(255),
-  description VARCHAR(255),
+  h1_content VARCHAR(255),
+  title_content VARCHAR(255),
+  description_content VARCHAR(255),
   created_at TIMESTAMP
 );
