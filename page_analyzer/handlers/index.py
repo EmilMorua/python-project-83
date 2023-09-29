5fcd76dc4bda5603bd5dce6e5ef9b6d53b575f8c
EmilMorua <emil.morua@gmail.com>
2023-09-29 09:23:44 +0500

    update index.py

--- page_analyzer/handlers/index.py
+++ page_analyzer/handlers/index.py
@@ -1,10 +1,10 @@
-from flask import render_template, redirect, url_for
+from flask import render_template, redirect, url_for, request
 from page_analyzer.forms import URLForm
 
 
 def index_handler():
     form = URLForm()
-    if form.validate_on_submit():
-        url = form.url.data
+    if request.method == 'POST':
+        url = request.form['url']
         return redirect(url_for('urls_handler', url=url))
     return render_template('index.html', form=form)
