3359bd9d4834071123dd394b3b0726547aa84ce6
EmilMorua <emil.morua@gmail.com>
2023-09-29 09:24:58 +0500

    update urls.py

--- page_analyzer/handlers/urls.py
+++ page_analyzer/handlers/urls.py
@@ -1,19 +1,100 @@
 from flask import render_template, redirect, url_for, flash, request
 from validators import url as validate_url
-from page_analyzer.app import app
+import psycopg2
+import os
+from datetime import datetime
 
-from page_analyzer.extensions import db
-from page_analyzer.forms import URLForm
-from page_analyzer.models import Url, UrlCheck
+from page_analyzer.app import app
+from page_analyzer.models import Url
+
+
+DATABASE_URL = os.getenv('DATABASE_URL')
+
+
+def get_last_checks(urls):
+    last_checks = {}
+    with app.app_context():
+        connection = psycopg2.connect(DATABASE_URL)
+        cur = connection.cursor()
+        for url in urls:
+            cur.execute(
+                "SELECT created_at, status_code FROM url_checks "
+                "WHERE url_id = %s "
+                "ORDER BY created_at DESC "
+                "LIMIT 1",
+                (url.id,)
+            )
+            row = cur.fetchone()
+            if row:
+                last_checks[url.id] = {
+                    'created_at': row[0].strftime('%Y-%m-%d'),
+                    'status_code': row[1]
+                }
+        cur.close()
+        connection.close()
+    return last_checks
+
+
+def is_valid_url(url):
+    return bool(url) and validate_url(url) and len(url) <= 255
+
+
+def find_existing_url_id(url, connection):
+    with connection.cursor() as cur:
+        cur.execute("SELECT id FROM urls WHERE name = %s", (url,))
+        existing_url = cur.fetchone()
+        return existing_url[0] if existing_url else None
+
+
+def add_new_url_to_database(url, connection):
+    with connection.cursor() as cur:
+        cur.execute(
+            "INSERT INTO urls (name, created_at) VALUES (%s, %s) "
+            "RETURNING id",
+            (url, datetime.utcnow())
+        )
+        return cur.fetchone()[0]
+
+
+def handle_url_post(form):
+    url = form['url']
+
+    if not is_valid_url(url):
+        flash('Некорректный URL', 'error')
+        return redirect(url_for('index_handler'))
+
+    with app.app_context():
+        connection = psycopg2.connect(DATABASE_URL)
+        try:
+            existing_url_id = find_existing_url_id(url, connection)
+            if existing_url_id:
+                flash('Страница уже существует', 'info')
+                return redirect(url_for('url_detail_handler',
+                                        id=existing_url_id))
+
+            new_url_id = add_new_url_to_database(url, connection)
+            connection.commit()
+            flash('Страница успешно добавлена', 'success')
+            return redirect(url_for('url_detail_handler', id=new_url_id))
+        finally:
+            connection.close()
 
 
 @app.route('/urls', methods=['GET', 'POST'])
 def urls_handler():
-    form = URLForm()
+    form = request.form
 
     if request.method == 'GET':
-        urls = Url.query.order_by(Url.created_at.desc()).all()
-        last_checks = get_last_checks(urls)
+        with app.app_context():
+            connection = psycopg2.connect(DATABASE_URL)
+            cur = connection.cursor()
+            cur.execute("SELECT * FROM urls ORDER BY created_at DESC")
+            urls_data = cur.fetchall()
+            urls = [Url(id=row[0], name=row[1], created_at=row[2])
+                    for row in urls_data]
+            cur.close()
+            connection.close()
+            last_checks = get_last_checks(urls)
         return render_template(
             'urls.html',
             form=form,
@@ -25,39 +106,3 @@ def urls_handler():
         return handle_url_post(form)
 
     return render_template('urls.html', form=form)
-
-
-def get_last_checks(urls: list[Url]) -> dict[int, dict[str, int]]:
-    last_checks = {}
-    for url in urls:
-        last_check = UrlCheck.query.filter_by(url_id=url.id)\
-            .order_by(UrlCheck.created_at.desc())\
-            .first()
-        if last_check:
-            last_checks[url.id] = {
-                'created_at': last_check.created_at.date(),
-                'status_code': last_check.status_code
-            }
-    return last_checks
-
-
-def handle_url_post(form: URLForm) -> str:
-    url = form.url.data
-    if not url:
-        flash('URL обязателен', 'error')
-        return redirect(url_for('index_handler'))
-
-    if validate_url(url) and len(url) <= 255:
-        existing_url = Url.query.filter_by(name=url).first()
-        if existing_url:
-            flash('Страница уже существует', 'info')
-            return redirect(url_for('url_detail_handler', id=existing_url.id))
-    else:
-        flash('Некорректный URL', 'error')
-        return redirect(url_for('index_handler'))
-
-    new_url = Url(name=url)
-    db.session.add(new_url)
-    db.session.commit()
-    flash('Страница успешно добавлена', 'success')
-    return redirect(url_for('url_detail_handler', id=new_url.id))
